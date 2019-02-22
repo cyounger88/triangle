0596f134610f073854934155cce1076521090c22
Chris <christophernyounger@gmail.com>
2019-02-21 18:39:27 -0800

    adds if statement to determine if triangle

--- js/scripts.js
+++ js/scripts.js
@@ -27,11 +27,15 @@ $(document).ready(function() {
     var side2 = parseInt($("#side2").val());
     var side3 = parseInt($("#side3").val());
 
-    if (equilateral%2 == 1) {
-      console.log("yes it is")
+    if ((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1)) {
+      console.log("its a triangle!")
+      var cosA = Math.cos((Math.pow(side2,2) + Math.pow(side3,2) - Math.pow(side1,2)) /  (2*(side2*side3)))
+      console.log(cosA);
     } else {
-      console.log("nahhh")
+      console.log("Not a triangle")
     }
+
+    
 
 
   });
